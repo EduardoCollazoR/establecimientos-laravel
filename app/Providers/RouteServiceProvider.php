d231e33215d2bb20890171fcdf365a4be30113d4
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2021-12-26 19:13:05 -0800

    sedder

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/establecimientos/create';
 
     /**
      * The controller namespace for the application.
